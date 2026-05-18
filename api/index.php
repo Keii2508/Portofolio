121c4ac20aec0ae6ba36287bba0f3f9208147408
Keii2508 <joseaustin2508@gmail.com>
2026-05-18 15:45:45 +0700

    update api 2

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+// rest of your code below...
 // Fix storage for Vercel read-only filesystem
 foreach ([
     '/tmp/storage/app/public',
